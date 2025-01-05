71232e8044877f82c2f4a592c677af94ce0ce6c2
Orlando <espadachinkinno@gmail.com>
2025-01-05 13:46:12 -0600

    Se agregan props al componente tweet

--- js/componentes.js
+++ js/componentes.js
@@ -4,9 +4,51 @@ class Tweet extends HTMLElement{
         this.attachShadow({mode: "open"})
     }
 
+    static get observedAttributes(){
+        return ["imagenPerfil", "nombre", "nombreUsuario", "tweet"]
+    }
+
     async connectedCallback(){
         const shadow = this.shadowRoot
         shadow.innerHTML = await this.getTemplate()
+
+        this.imagenPerfil = shadow.querySelector("img")
+        this.nombre = shadow.querySelector("h3")
+        this.nombreUsuario = shadow.querySelector("p")
+        this.tweet = shadow.querySelector(".tweet")
+
+        this.imagenPerfil.src = this.getAttribute("imagenPerfil")
+        this.nombre.innerText = this.getAttribute("nombre")
+        this.nombreUsuario.innerText = this.getAttribute("nombreUsuario")
+        this.tweet.innerText = this.getAttribute("tweet")
+
+    }
+
+    attributeChangedCallback(name, oldValue, newValue){
+        switch(name){
+            case "imagenPerfil":
+                if (this.imagenPerfil){
+                    return this.imagenPerfil.src = newValue
+                }
+                break
+            case "nombre":
+                if(this.nombre){
+                    return this.nombre.innerText = newValue
+                }
+                break
+            case "nombreUsuario":
+                if(this.nombreUsuario){
+                    return this.nombreUsuario.innerText = newValue
+                }
+                break
+            case "tweet":
+                if(this.tweet){
+                    return this.tweet.innerText = newValue
+                }
+                break
+            default:
+                break
+        }
     }
 
     async getTemplate(){
