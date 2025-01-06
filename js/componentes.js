14c54ad43a22cfea751b5eb7856133999cbbb464
Orlando <espadachinkinno@gmail.com>
2025-01-05 22:50:33 -0600

    segunda prueba

--- js/componentes.js
+++ js/componentes.js
@@ -1,60 +1,81 @@
-class Tweet extends HTMLElement{
-    constructor(){
-        super()
-        this.attachShadow({mode: "open"})
-    }
+// class Tweet extends HTMLElement{
+//     constructor(){
+//         super()
+//         this.attachShadow({mode: "open"})
+//     }
 
-    static get observedAttributes(){
-        return ["imagenPerfil", "nombre", "nombreUsuario", "tweet"]
-    }
+//     static get observedAttributes(){
+//         return ["imagenPerfil", "nombre", "nombreUsuario", "tweet"]
+//     }
 
-    async connectedCallback(){
-        const shadow = this.shadowRoot
-        shadow.innerHTML = await this.getTemplate()
+//     async connectedCallback(){
+//         const shadow = this.shadowRoot
+//         shadow.innerHTML = await this.getTemplate()
 
-        this.imagenPerfil = shadow.querySelector("img")
-        this.nombre = shadow.querySelector("h3")
-        this.nombreUsuario = shadow.querySelector("p")
-        this.tweet = shadow.querySelector(".tweet")
+//         this.imagenPerfil = shadow.querySelector("img")
+//         this.nombre = shadow.querySelector("h3")
+//         this.nombreUsuario = shadow.querySelector("p")
+//         this.tweet = shadow.querySelector(".tweet")
 
-        this.imagenPerfil.src = this.getAttribute("imagenPerfil")
-        this.nombre.innerText = this.getAttribute("nombre")
-        this.nombreUsuario.innerText = this.getAttribute("nombreUsuario")
-        this.tweet.innerText = this.getAttribute("tweet")
+//         this.imagenPerfil.src = this.getAttribute("imagenPerfil")
+//         this.nombre.innerText = this.getAttribute("nombre")
+//         this.nombreUsuario.innerText = this.getAttribute("nombreUsuario")
+//         this.tweet.innerText = this.getAttribute("tweet")
 
-    }
+//     }
 
-    attributeChangedCallback(name, oldValue, newValue){
-        switch(name){
-            case "imagenPerfil":
-                if (this.imagenPerfil){
-                    return this.imagenPerfil.src = newValue
-                }
-                break
-            case "nombre":
-                if(this.nombre){
-                    return this.nombre.innerText = newValue
-                }
-                break
-            case "nombreUsuario":
-                if(this.nombreUsuario){
-                    return this.nombreUsuario.innerText = newValue
-                }
-                break
-            case "tweet":
-                if(this.tweet){
-                    return this.tweet.innerText = newValue
-                }
-                break
-            default:
-                break
-        }
-    }
+//     attributeChangedCallback(name, oldValue, newValue){
+//         switch(name){
+//             case "imagenPerfil":
+//                 if (this.imagenPerfil){
+//                     return this.imagenPerfil.src = newValue
+//                 }
+//                 break
+//             case "nombre":
+//                 if(this.nombre){
+//                     return this.nombre.innerText = newValue
+//                 }
+//                 break
+//             case "nombreUsuario":
+//                 if(this.nombreUsuario){
+//                     return this.nombreUsuario.innerText = newValue
+//                 }
+//                 break
+//             case "tweet":
+//                 if(this.tweet){
+//                     return this.tweet.innerText = newValue
+//                 }
+//                 break
+//             default:
+//                 break
+//         }
+//     }
 
-    async getTemplate(){
-        const archivo= await fetch("../componentes/tweets.html")
-        const plantilla = await archivo.text()
-        return plantilla
+//     async getTemplate(){
+//         const archivo= await fetch("../componentes/tweets.html")
+//         const plantilla = await archivo.text()
+//         return plantilla
+//     }
+// }
+
+class TweetUser extends HTMLElement {
+    connectedCallback() {
+        const imagenPerfil = this.getAttribute('imagenPerfil');
+        const nombre = this.getAttribute('nombre');
+        const nombreUsuario = this.getAttribute('nombreUsuario');
+        const tweet = this.getAttribute('tweet');
+
+        this.innerHTML = `
+            <div class="tweet">
+                <figure>
+                    <img src="${imagenPerfil}" alt="${nombre}" class="perfil-imagen">
+                </figure>
+                <div>
+                    <h3>${nombre} <span>${nombreUsuario}</span></h3>
+                    <p>${tweet}</p>
+                </div>
+            </div>
+        `;
     }
 }
 
@@ -94,7 +115,7 @@ class HeaderInline extends HTMLElement{
     }
 }
 
-customElements.define("tweet-user", Tweet)
+customElements.define("tweet-user", TweetUser)
 customElements.define("header-app", HeaderUp)
 customElements.define("header-inline", HeaderInline)
 
