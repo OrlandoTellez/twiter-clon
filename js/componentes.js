309d9c22d9c85d9596d9e1573729a4186282e331
Orlando <espadachinkinno@gmail.com>
2025-01-27 17:47:52 -0600

    Se soluciona problema del slider

--- js/componentes.js
+++ js/componentes.js
@@ -1,18 +1,18 @@
 class TweetUser extends HTMLElement {
   constructor() {
-    super();
+    super()
   }
   async connectedCallback() {
-    const imagenPerfil = this.getAttribute("imagenPerfil") || "";
-    const nombre = this.getAttribute("nombre") || "";
-    const nombreUsuario = this.getAttribute("nombreUsuario") || "";
-    const tweet = this.getAttribute("tweet") || "";
-    const imagenTweet = this.getAttribute("imagenTweet") || "";
+    const imagenPerfil = this.getAttribute("imagenPerfil") || ""
+    const nombre = this.getAttribute("nombre") || ""
+    const nombreUsuario = this.getAttribute("nombreUsuario") || ""
+    const tweet = this.getAttribute("tweet") || ""
+    const imagenTweet = this.getAttribute("imagenTweet") || ""
 
     try {
-      const response = await fetch("../componentes/tweets.html");
+      const response = await fetch("../componentes/tweets.html")
       if (!response.ok) {
-        throw new Error("No se pudo cargar el archivo HTML");
+        throw new Error("No se pudo cargar el archivo HTML")
       }
 
       const template = await response.text();
@@ -24,7 +24,7 @@ class TweetUser extends HTMLElement {
         .replace("{{tweet}}", tweet)
         .replace("{{imagenTweet}}", imagenTweet)
     } catch (error) {
-      console.error("Error cargando el archivo HTML:", error);
+      console.error("Error cargando el archivo HTML:", error)
     }
   }
 }
@@ -34,8 +34,8 @@ class HeaderUp extends HTMLElement {
     super();
   }
   async connectedCallback() {
-    const response = await fetch("../componentes/header-up.html");
-    this.innerHTML = await response.text();
+    const response = await fetch("../componentes/header-up.html")
+    this.innerHTML = await response.text()
   }
 }
 
@@ -44,11 +44,11 @@ class HeaderInline extends HTMLElement {
     super();
   }
   async connectedCallback() {
-    const response = await fetch("../componentes/header-inline.html");
-    this.innerHTML = await response.text();
+    const response = await fetch("../componentes/header-inline.html")
+    this.innerHTML = await response.text()
   }
 }
 
-customElements.define("tweet-user", TweetUser);
-customElements.define("header-up", HeaderUp);
-customElements.define("header-inline", HeaderInline);
+customElements.define("tweet-user", TweetUser)
+customElements.define("header-up", HeaderUp)
+customElements.define("header-inline", HeaderInline)
