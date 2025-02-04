4506f0a8be4657c6d73ff2ddc8822e0439310f25
Orlando <espadachinkinno@gmail.com>
2025-02-04 00:17:29 -0600

    middleware de la autenticacion

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+export const authenticate = (req, res, next) => {
+    const token = req.header("Authorization")?.replace("Bearer ", "")
+
+    if (!token) {
+        return res.status(401).json({ error: "Acceso no autorizado" })
+    }
+
+    try{
+        const decoded  = jwt.verify(token, JWT_SECRET)
+        req.userId = decoded.userId
+        next()
+    }catch(error){
+        return res.status(401).json({ error: "Token invalido" })
+    }
+}
