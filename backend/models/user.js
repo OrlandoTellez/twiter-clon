c410c6eb50a8559485b84c548c0a142081d0e590
Orlando <espadachinkinno@gmail.com>
2025-02-04 00:16:28 -0600

    configuracion de la base de datos y del modelo de crear y encontrar usuarios

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+import pool from "../config/db"
+
+export default class User {
+    static async create({email, password}){
+        const [result] = await pool.execute("INSERT INTO users (email, password) VALUES (?, ?)", [email, password])
+
+        return result.insertId
+    }
+
+    static async findByEmail(email){
+        const[rows] = await pool.execute("SELECT * FROM users WHERE email = ?", [email])
+        return rows[0]
+    }
+}
+
