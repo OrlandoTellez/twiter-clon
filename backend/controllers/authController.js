78b46af8125c507818cd831761a713ec948f41c1
Orlando <espadachinkinno@gmail.com>
2025-02-04 00:17:08 -0600

    controlador de la autenticacion

---
+++ backend/controllers/authController.js
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/user.js"
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+export const register = async (req, res) => {
+    try{
+        const {email, password} = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const userId = await User.create({email, password: hashedPassword})
+
+        res.status(201).json({message: "usuario registrado exitosamente", userId})
+    }catch(error){
+        res.statu(500).json({error: "Error al registrar usuario"})
+    }
+}
+
+
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body
+        const user = await User.findByEmail(email)
+
+        if(!user || !(await bcrypt.compare(password, user.password))){
+            return res.status(401).json({error: "Credenciales incorrectas"})
+        }
+
+        const token = jwt.sign({userId: user.id}, JWT_SECRET, {expiresIn: "1h"})
+        res.json({token})
+    }catch(error){
+        res.status(500).json({error: "Error al iniciar sesion"})
+    }
+}
