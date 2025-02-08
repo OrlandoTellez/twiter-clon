021a7e57e7594fa10d26ea544df3bcd51ad32203
Orlando <espadachinkinno@gmail.com>
2025-02-07 19:04:27 -0600

    configuracion db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,15 +3,14 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const pool = mysql.createPool({
+const dbConfig = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-})
+    database: process.env.DB_NAME
+} 
+
+const pool = mysql.createPool(dbConfig)
 
 export default pool
