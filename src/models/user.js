d793147caec60f77f32c68acb63cd37da2ff2a63
Orlando <espadachinkinno@gmail.com>
2025-02-15 22:17:29 -0600

    se maneja mejor el encontrar un email, si no lo encuentra devuelve null

--- src/models/user.js
+++ src/models/user.js
@@ -9,7 +9,7 @@ export default class User {
 
     static async findByEmail(email){
         const[rows] = await pool.execute("SELECT * FROM users WHERE email = ?", [email])
-        return rows[0]
+        return rows[0].lenght > 0 ? rows[0] : null
     }
 }
 
