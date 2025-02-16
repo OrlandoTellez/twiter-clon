17598955f6e9708a2c0d32866ddde77464f76bd6
Orlando <espadachinkinno@gmail.com>
2025-02-15 22:22:57 -0600

    desde el el modelo user se maneja el error si el email ya esta registrado, para evitar errores en sql y manejarlos desde el backen

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,12 @@ import pool from "../config/db.js"
 
 export default class User {
     static async create({username, email, password}){
+        const emailExists = await User.findByEmail(email)
+
+        if(emailExists){
+            throw new Error("Email ya registrado")
+        }
+
         const [result] = await pool.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", [username, email, password])
 
         return result.insertId
