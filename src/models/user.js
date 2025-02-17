22ba31b195024d2d7eb8424ff15a48c0eababf1c
Orlando <espadachinkinno@gmail.com>
2025-02-16 21:24:22 -0600

    se cambia modelo para registrar usuario

--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,26 @@
 import pool from "../config/db.js"
 
 export default class User {
-    static async create({username, email, password}){
+    static async create({nombre_usuario, nombre, apellido, email, password}){
         const emailExists = await User.findByEmail(email)
 
         if(emailExists){
             throw new Error("Email ya registrado")
         }
 
-        const [result] = await pool.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", [username, email, password])
+        const [result] = await pool.execute("INSERT INTO usuarios (nombre_usuario, nombre, apellido, email, password) VALUES (?, ?, ?, ?, ?)", [nombre_usuario, nombre, apellido, email, password])
 
         return result.insertId
     }
 
     static async findByEmail(email){
-        const[rows] = await pool.execute("SELECT * FROM users WHERE email = ?", [email])
+        const[rows] = await pool.execute("SELECT * FROM usuarios WHERE email = ?", [email])
+        return rows.length > 0 ? rows[0] : null
+    }
+
+    static async findById(id){
+        const [rows] = await pool.execute("SELECT * FROM usuarios WHERE id = ?", [id])
+
         return rows.length > 0 ? rows[0] : null
     }
 }
