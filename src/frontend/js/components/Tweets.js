1fec7ecc3d328bab39c03868420c8ccb3612ab81
Orlando <espadachinkinno@gmail.com>
2025-02-23 20:23:49 -0600

    refactor(tweets): usar js en vez de web component para renderizar tweets

---
+++ src/frontend/js/components/Tweets.js
@@ -0,0 +1,34 @@
+async function publicarTweets(contenedor, atributos) {
+    try {
+        const response = await fetch("../../componentes/tweets.html")
+        if (!response.ok) throw new Error("No se pudo cargar el archivo HTML")
+
+        let template = await response.text()
+        template = template
+            .replace("{{imagenPerfil}}", atributos.imagenPerfil || "")
+            .replace("{{nombre}}", atributos.nombre || "")
+            .replace("{{nombreUsuario}}", atributos.nombreUsuario || "")
+            .replace("{{tweet}}", atributos.tweet || "")
+            .replace("{{imagenTweet}}", atributos.imagenTweet || "")
+
+        const tweetElement = document.createElement("div")
+        tweetElement.classList.add("tweet")
+        tweetElement.innerHTML = template
+        contenedor.prepend(tweetElement)
+    } catch (error) {
+        console.error("Error cargando el archivo HTML:", error)
+    }
+}
+const botonTweet = document.querySelector(".btn-postear")
+const inputTweet = document.querySelector(".tweet-input")
+const contenedorTweets = document.querySelector(".tweet-container--index")
+
+botonTweet.addEventListener("click", () => {
+    publicarTweets(contenedorTweets, {
+        imagenPerfil: "https://unavatar.io/x/1saf21asd21asdf2",
+        nombre: "IbaiLlanos",
+        nombreUsuario: "@IbaiLlanos",
+        tweet: inputTweet.value,
+        imagenTweet: ""
+    })
+})
