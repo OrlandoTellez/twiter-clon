5d7a54007983bba5f2ed4aae606ce2e527306093
OrlandoTellez <espadachinkinno@gmail.com>
2026-01-02 15:01:42 -0600

    feat: agregar funcion para encodear el jwt token y tambien la constante jwt_secret

--- backend/src/config/constants.rs
+++ backend/src/config/constants.rs
@@ -4,3 +4,6 @@ use once_cell::sync::Lazy;
 
 pub static DATABASE_URL: Lazy<String> =
     Lazy::new(|| env::var("DATABASE_URL").expect("Se necesita la variable de entorno"));
+
+pub static JWT_SECRET: Lazy<String> =
+    Lazy::new(|| env::var("JWT_SECRET").expect("Se necesita la variable de entorno"));
---
+++ backend/src/helpers/jwt.rs
@@ -0,0 +1,24 @@
+use chrono::{Duration, Utc};
+use jsonwebtoken::{EncodingKey, Header, encode};
+
+use crate::{config::constants::JWT_SECRET, helpers::errors::AppError, models::auth_model::Claim};
+
+pub fn enconde(username: String) -> Result<String, AppError> {
+    let expiration = Utc::now()
+        .checked_add_signed(Duration::hours(24))
+        .expect("Valid timestamp")
+        .timestamp();
+
+    let claims: Claim = Claim {
+        sub: username,
+        exp: expiration as usize,
+        iat: Utc::now().timestamp() as usize,
+    };
+
+    encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(JWT_SECRET.as_bytes()),
+    )
+    .map_err(|_| AppError::InternalServerError("Error encode jwt".to_string()))
+}
